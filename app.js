88f2f8921e9f9c436ab5bcde65b046bdcb4d3ebb
prathmeshwake <prathmeshwake123@gmail.com>
2026-01-09 16:07:54 +0530

    Test GitHub Actions deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from OpenShift Sandbox + GitHub Actions!');
+  res.send('Hello Updated from GitHub Actions!');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
